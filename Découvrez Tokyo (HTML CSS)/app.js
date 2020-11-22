7f50a315335ecf2d526cd48bdc2da3cf3a340513
Eddy-Pereira <eddy.pereirapro@gmail.com>
2020-11-22 22:42:06 +0100

    Add files via upload

---
+++ Découvrez Tokyo (HTML CSS)/app.js
@@ -0,0 +1,14 @@
+const btnMenu = document.querySelector('.logo-menu');
+const menu = document.querySelector('.liste-nav');
+
+btnMenu.addEventListener('click',function(){
+    menu.classList.toggle('active');
+})
+
+
+
+const allLinks = document.querySelectorAll('.item-nav');
+
+allLink.forEach(functio('clck', function() {
+    menu.classList.toggle('active');
+})
